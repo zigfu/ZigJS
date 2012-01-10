55940513c5f657dac3fe8851978e6b16950a9db3
Roee Shenberg <roee@zigfu.com>
2012-01-09 19:53:23 -0800

    updated for latest zigmote code

--- musicv3/zigmote-client.js
+++ musicv3/zigmote-client.js
@@ -1,4 +1,6 @@
 var zigmote = (function() {
+
+	plugins = {}
 	
 	function host(socket) {
 		var location = {}; // TODO : fill me in!
@@ -17,9 +19,30 @@ var zigmote = (function() {
 			unlock : unlock,
 		};
 
-		function startRoom(roomid, name, successcb) {
+		function setCookie(c_name,value,exdays) {
+			var exdate=new Date();
+			exdate.setDate(exdate.getDate() + exdays);
+			var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+			document.cookie=c_name + "=" + c_value;
+		}
+
+		function getCookie(c_name) {
+			var i,x,y,ARRcookies=document.cookie.split(";");
+			for (i=0;i<ARRcookies.length;i++) {
+			  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+			  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+			  x=x.replace(/^\s+|\s+$/g,"");
+			  if (x==c_name) {
+			    return unescape(y);
+			  }
+			}
+		}
+		
+		function startRoom(name, successcb) {
 
 			socket.on('zigmote-roomStarted', function(data) {
+				// save roomid in cookie
+				setCookie('zigmote-roomid', data.roomid);
 				successcb(data);
 
 				socket.on('zigmote-sendToHost', function(data) {
@@ -47,6 +70,9 @@ var zigmote = (function() {
 					}
 				})	
 			});
+
+			// get cookie with roomid
+			var roomid = getCookie('zigmote-roomid');
 
 			socket.emit('zigmote-startRoom', {
 				roomid : roomid,
@@ -87,6 +113,13 @@ var zigmote = (function() {
 
 		function unlock(lockid) {
 			socket.emit('zigmote-unlock', { lockid : lockid });
+		}
+
+		// "install" plugins
+		for (var plugin in plugins) if (plugins.hasOwnProperty(plugin)) {
+			if (plugins[plugin].hasOwnProperty("host")) {
+				ret = plugins[plugin].host(ret);
+			}
 		}
 
 		return ret;
@@ -174,6 +207,13 @@ var zigmote = (function() {
 			socket.emit('zigmote-unlock', { lockid : lockid });	
 		}
 
+		// "install" plugins
+		for (var plugin in plugins) if (plugins.hasOwnProperty(plugin)) {
+			if (plugins[plugin].hasOwnProperty("controller")) {
+				ret = plugins[plugin].controller(ret);
+			}
+		}
+
 		return ret;
 	}
 
@@ -181,6 +221,7 @@ var zigmote = (function() {
 	return {
 		host : host,
 		controller : controller,
+		plugins : plugins,
 		roomslistid : "roomslist",
 	}
 }());
