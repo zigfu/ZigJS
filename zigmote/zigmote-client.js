67d9af1134c5146bca92dc77ed0be3e8388908f7
shlomo.zippel <shlomo@zigfu.com>
2012-01-09 18:40:33 -0800

    initial session support

--- zigmote/zigmote-client.js
+++ zigmote/zigmote-client.js
@@ -17,9 +17,30 @@ var zigmote = (function() {
 			unlock : unlock,
 		};
 
-		function startRoom(roomid, name, successcb) {
+		function setCookie(c_name,value,exdays) {
+			var exdate=new Date();
+			exdate.setDate(exdate.getDate() + exdays);
+			var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+			document.cookie=c_name + "=" + c_value;
+		}
+
+		function getCookie(c_name) {
+			var i,x,y,ARRcookies=document.cookie.split(";");
+			for (i=0;i<ARRcookies.length;i++) {
+			  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+			  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+			  x=x.replace(/^\s+|\s+$/g,"");
+			  if (x==c_name) {
+			    return unescape(y);
+			  }
+			}
+		}
+		
+		function startRoom(name, successcb) {
 
 			socket.on('zigmote-roomStarted', function(data) {
+				// save roomid in cookie
+				setCookie('zigmote-roomid', data.roomid);
 				successcb(data);
 
 				socket.on('zigmote-sendToHost', function(data) {
@@ -47,6 +68,9 @@ var zigmote = (function() {
 					}
 				})	
 			});
+
+			// get cookie with roomid
+			var roomid = getCookie('zigmote-roomid');
 
 			socket.emit('zigmote-startRoom', {
 				roomid : roomid,
