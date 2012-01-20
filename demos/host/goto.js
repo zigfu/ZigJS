85b074d2dfa1310c447d24fd45269d2156ae8798
shlomo.zippel <shlomo@zigfu.com>
2012-01-19 21:38:45 -0800

    missing assets

---
+++ demos/host/goto.js
@@ -0,0 +1,19 @@
+var goto = (function() {
+	var prefix = "/host/"
+
+	paths = {
+		home : "index.html",
+		music : "musicv3/index.html",
+		jsnes : "jsnes/index.html",
+		youtube : "youtubev2/index.html",
+		slides : "slideviewer/slides.html"
+	}
+
+	function goto(page) {
+		if (!paths.hasOwnProperty(page)) return;
+		var url = prefix + paths[page];
+		document.location.pathname = url;
+	}
+
+	return goto;
+}())
